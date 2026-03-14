9e2d6f64df9d443807e6f8fde4cfc1674a10e730
Keilthar <keilthar.gaming@gmail.com>
2026-03-14 13:33:17 +0100

    Quartz sync: Mar 14, 2026, 1:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Keilthar — Technical Devlog",
+    pageTitleSuffix: " | Keilthar",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "Keilthar.github.io/devlog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
